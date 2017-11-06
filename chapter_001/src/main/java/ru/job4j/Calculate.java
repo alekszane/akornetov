240313c3e9f7ac9834be0e239dda2492b7469a60
Aleksey Kornetov <all-1313@yandex.ru>
2017-11-07 01:36:07 +0300

    1.4. JUnit

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -15,4 +15,13 @@ public class Calculate{
 		
 	}
 	
+/**
+* Method echo.
+* @param name Your name.
+* @return Echo plus your name.
+*/
+public String echo(String name) {
+    return "Echo, echo, echo : " + name;
+}
+	
 }
---
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -0,0 +1,28 @@
+package ru.job4j;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test.
+*
+* @author Aleksey Kornetov (all-1313@yandex.ru)
+* @version $Id$
+* @since 0.1
+*/
+public class CalculateTest {
+/**
+* Test echo.
+@Test*/ 
+public void whenTakeNameThenTreeEchoPlusName() {
+    String input = "Aleksey Kornetov";
+    String expect = "Echo, echo, echo: Aleksey Kornetov"; 
+    Calculate calc = new Calculate();
+    String result = calc.echo(input);
+    assertThat(result, is(expect));
+}
+ 
+}
