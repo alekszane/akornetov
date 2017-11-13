956e82f999f57a5e2fbba6f08056d2e7a1d68eed
Aleksey Kornetov <all-1313@yandex.ru>
2017-11-13 20:52:41 +0300

    2.3 Элементарный калькуляторЭ

--- chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
+++ chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
@@ -1,8 +1,5 @@
 package ru.job4j.calculator;
 
-import com.sun.org.apache.xerces.internal.util.PropertyState;
-import org.junit.Test;
-
 import static com.sun.org.apache.xerces.internal.util.PropertyState.is;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
