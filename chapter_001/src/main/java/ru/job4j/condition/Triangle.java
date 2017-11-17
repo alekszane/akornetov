c2c5701acf1c13ea4fe59bc50cffd2ecd731a744
Aleksey Kornetov <all-1313@yandex.ru>
2017-11-18 01:52:36 +0300

    Вычисление площади треугольника

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -3,13 +3,12 @@ package ru.job4j.condition;
  * Class Triangle
  * @author Aleksey Kornetov (all-1313@yandex.ru)
  */
-
 public class Triangle {
     private Point a;
     private Point b;
     private Point c;
     // Определяем точки
-    public Triangle(Point a, Point b, Point c) {
+    public Triangle (Point a, Point b, Point c) {
         this.a = a;
         this.b = b;
         this.c = c;
@@ -20,16 +19,17 @@ public class Triangle {
      * @return расстояние между точками left и right.
      */
     public double distance(Point left, Point right) {
-        return Math.sqrt((x: this.b - x: this.a)^2 + (y: this.b - y:this.a)^2);
+        return Math.sqrt(
+                Math.pow(left.getX() - right.getX()) + Math.pow(left.getY() - right.getY()));
     }
     /** Метод вычисления периметра по длинам сторон
-    * @param ab расстояние между точками a b
-    * @param ac расстояние между точками a c
-    * @param bc расстояние между точками b c
-    * @return Периметр.
-    */
+     * @param ab расстояние между точками a b
+     * @param ac расстояние между точками a c
+     * @param bc расстояние между точками b c
+     * @return Периметр.
+     */
     public double period(double ab, double ac, double bc) {
-        return (this.a * this.b + this.a * this.c + this.b * this.c) / 2;
+        return (ab + ac +bc) / 2;
     }
     /*
      * Метод должен вычислить прощадь треугольника.
@@ -42,8 +42,8 @@ public class Triangle {
         double bc = this.distance(this.b, this.c);
         double p = this.period(ab, ac, bc);
         if (this.exist(ab, ac, bc)) {
-            // написать формулу для расчета площади треугольника.
-            //rsl = ...
+            //  формула для расчета площади треугольника.
+            rsl = Math.sqrt( p *(p - ab) * (p - ac) * (p - bc));
         }
         return rsl;
     }
@@ -56,6 +56,10 @@ public class Triangle {
      * @return
     */
     private boolean exist(double ab, double ac, double bc) {
-        return false;
+        if(ab + ac > bc && ac + bc > ab && ab + bc > ac && ab > 0 && ac > 0 && bc > 0){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
