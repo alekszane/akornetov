18fc0e8edada05b87fa60463963da512334767ee
Aleksey Kornetov <all-1313@yandex.ru>
2017-11-18 02:09:11 +0300

    Исправление Point.java

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -1,8 +1,8 @@
 package ru.job4j.condition;
 
 public class Point {
-	private int x;
-	private int y;
+	public int x;
+	public int y;
 
 	public Point(int x, int y) {
 		this.x = x;
@@ -17,7 +17,6 @@ public class Point {
 		return this.y;
 	}
 
-	public boolean is(int a, int b) {
-        return (this.y == this.x * a + b) ? true : false;
-		}
-	}
+	public boolean is(int a, int b) {return true;}//(this.y == this.x * a + b) ? true : false;}
+}
+
