92729c2d8afb9c3c38f70ffdb6f9183052850f28
Aleksey Kornetov <all-1313@yandex.ru>
2017-11-19 20:38:44 +0300

    Максимум из 3-х точек выполнено

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -1,7 +1,13 @@
 package ru.job4j.max;
 /*Класс Max*/
 public class Max {
+    //Метод возвращает максимум из 2х чисел
     public int max(int first, int second) {
         return (first < second) ? second : first;
     }
-}
+    //Метод возвращает максимум из 3-х чисел
+   public int max(int first, int second, int third) {
+        int res = max(third, max(first, second));
+        return res;
+        }
+    }
--- chapter_001/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -8,10 +8,20 @@ public class MaxTest {
 	/**
      * Тест Max
      */
+	//Создаем объект, и передаем в него числа
+	//assertThat Возвращает результат
 	@Test
 	public void whenFirstLessSecond() {
 		Max maxim = new Max();
 		int result = maxim.max(1, 2);
 		assertThat(result, is(2));
+	}
+	//Создаем объект, и передаем в него числа
+	//assertThat Возвращает результат
+	@Test
+	public void whenFirstMoreSecondAndThird() {
+		Max maxim = new Max();
+		int result = maxim.max(3, 2, 1);
+		assertThat(result, is(3));
 	}
 }
