2be4e77976650d46bff07a2896127ebcfd167fe6
Aleksey Kornetov <all-1313@yandex.ru>
2017-12-07 00:53:16 +0300

    Рефакторинг

---
+++ chapter_001/src/main/java/ru/job4j/string/Contains.java
@@ -0,0 +1,31 @@
+package ru.job4j.string;
+/**
+ * Created by All on 04.12.2017.
+ */
+public class Contains {
+	/**
+	 * @param origin оригинальная строка.
+	 * @param sub строка в строке.
+	 * @return true.
+	 */
+	boolean contains(String origin, String sub) {
+		char[] arrayOrigin = origin.toCharArray();
+		char[] arraySub = sub.toCharArray();
+		boolean result = false;
+
+		for (int i = 0; i < arrayOrigin.length; i++) {
+				int s;
+				for (s = 0; s < arraySub.length; s++) {
+					if (arraySub[s] != arrayOrigin[i+s]){
+						break;
+					}
+				}
+				//Если совпали все элементы  arraySub.
+				if (s == arraySub.length) {
+					result = true;
+					break;
+				}
+			}
+			return result;
+		}
+}
---
+++ chapter_001/src/test/java/ru/job4j/string/ContainsTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.string;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+ * Created by All on 04.12.2017.
+ */
+public class ContainsTest {
+	@Test
+	public void whenStringOriginContainIncludeStringSubThenTrue() {
+		Contains stringContains = new Contains();
+		boolean rsl = stringContains.contains("HELLO", "ELL");
+		assertThat(rsl, is(true));
+	}
+	@Test
+	public void whenStringOriginContainIncludeStringSubNewThenTrue() {
+		Contains stringContains = new Contains();
+		boolean rsl = stringContains.contains("ab cd ef g", "ef");
+		assertThat(rsl, is(true));
+	}
+	@Test
+	public void whenStringOriginContainIncludeStringSubNewThenFalse() {
+		Contains stringContains = new Contains();
+		boolean rsl = stringContains.contains("ab cd ef g", "xy");
+		assertThat(rsl, is(false));
+	}
+}
