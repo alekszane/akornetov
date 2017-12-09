f75dc0326bb982216622bbd27c9275bf94d2b1ac
Aleksey Kornetov <all-1313@yandex.ru>
2017-12-10 00:34:11 +0300

    Убрал ненужное условие

--- chapter_001/src/main/java/ru/job4j/array/ArrayCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayCheck.java
@@ -3,14 +3,12 @@ package ru.job4j.array;
  * Created by All on 08.12.2017.
  */
 public class ArrayCheck {
-	public boolean isSorted(int[] arr, boolean check) {
-		if (check == false) {
+	public boolean isSorted(int[] arr) {
 			for (int i = 0; i < arr.length - 1; ++i) {
 				if (arr[i] > arr[i + 1]) {
 					return false;
 				}
 			}
-		}
 		return true;
 	}
 }
