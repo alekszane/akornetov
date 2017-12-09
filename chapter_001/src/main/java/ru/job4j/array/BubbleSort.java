79d10e82dfff212efac27c6b5b3d343768c2ab2b
Aleksey Kornetov <all-1313@yandex.ru>
2017-12-09 12:18:57 +0300

    Добавление проверки сортировки массива

--- chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -5,13 +5,18 @@ package ru.job4j.array;
 public class BubbleSort {
 	public int[] sort(int[] array) {
 		for (int i = array.length - 1; i > 0; i--) {
+			boolean sort = true; //В переменной sort храним признак того, отсортирован массив или нет
 			for (int j = 0; j < i; j++) {
 				//Элементы сравниваются попарно, если порядок не правильный, меняем местами
 				if (array[j] > array[j + 1]) {
 					int a = array[j];
 					array[j] = array[j + 1];
 					array[j + 1] = a;
+					sort = false;
 				}
+			}
+			if(sort) { //Если отсортирован, тогда выходим из цикла
+				break;
 			}
 		}
 		return array;
