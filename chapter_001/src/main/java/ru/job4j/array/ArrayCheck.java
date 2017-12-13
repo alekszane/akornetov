767f89ce7b5ec60006796ee763e4aaea820cb422
Aleksey <all-1313@yandex.ru>
2017-12-13 22:49:06 +0300

    Рефакторинг

--- chapter_001/src/main/java/ru/job4j/array/ArrayCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayCheck.java
@@ -3,15 +3,13 @@ package ru.job4j.array;
  * Created by All on 08.12.2017.
  */
 public class ArrayCheck {
-	public boolean isSorted(int[] arr, boolean check) {
+	public boolean isSorted(int[] arr) {
 		boolean result = true;
-		if (check == false) {
 			for (int i = 0; i < arr.length - 1; ++i) {
 				if (arr[i] > arr[i + 1]) {
 					result = false;
 				}
 			}
-		}
 		return result;
 	}
 }
