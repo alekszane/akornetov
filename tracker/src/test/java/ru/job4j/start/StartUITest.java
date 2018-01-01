ef974ca0027332750b9a64fad39f310351f24872
Aleksey <all-1313@yandex.ru>
2018-01-02 00:11:22 +0300

    Написал тесты

--- tracker/src/test/java/ru/job4j/start/StartUITest.java
+++ tracker/src/test/java/ru/job4j/start/StartUITest.java
@@ -4,11 +4,6 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 import ru.job4j.model.Item;
-import ru.job4j.start.Input;
-import ru.job4j.start.StartUI;
-import ru.job4j.start.StubInput;
-import ru.job4j.start.Tracker;
-
 /**
  * Created by All on 29.12.2017.
  */
@@ -21,25 +16,21 @@ public class StartUITest {
     }
 // Метод тестирует добавление заявки пользователем
     @Test
-    public void whenUserAddItemThenTrackerHasNewItemWithNewName() {
-        Tracker tracker = new Tracker();
-        String[]one = {"0", "name", "desc", "1"};
-        Input input = new StubInput(one);
-        new StartUI(input, tracker).init();
-        assertThat(tracker.getAll()[0].getName(), is("name"));
+    public void whenUserAddItemThenTrackerHasNewItemWithSameName() {
+        Tracker tracker = new Tracker(); // создаём Tracker
+        Input input = new StubInput(new String[]{"0", "test name", "desc", "6"}); //создаём StubInput с последовательностью действий
+        new StartUI(input, tracker).init(); // создаём StartUI и вызываем метод init()
+        assertThat(tracker.getAll()[0].getName(), is("test name")); // проверяем, что нулевой элемент массива в трекере содержит имя, введённое при эмуляции.
     }
     // Метод тестирует изменение полей заявки
     @Test
     public void whenUpdateThenTrackerUpdateValue() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("name2", "desc2", 2);
-        tracker.add(item);
-        Item item2 = new Item("name3", "desc3", 3);
-        String[] two = {"2", "name2", "name3", "desc3"};
-        Input input = new StubInput(two);
-        new StartUI(input, tracker).init();
-        assertThat(tracker.getAll()[0].getName(), is("name3"));
-
+        Tracker tracker = new Tracker(); // создаём Tracker
+        Item item = new Item("name", "desk", 1);
+        tracker.add(item); //Напрямую добавляем заявку
+        Input input = new StubInput(new String[]{"2", item.getName(), "test name", "desc", "6"}); //создаём StubInput с последовательностью действий
+        new StartUI(input, tracker).init();  // создаём StartUI и вызываем метод init()
+        assertThat(tracker.getAll()[0].getName(), is("test name")); // проверяем, что нулевой элемент массива в трекере содержит имя, введённое при эмуляции.
     }
 
     @Test
@@ -49,8 +40,7 @@ public class StartUITest {
         tracker.add(item);
         Item item2 = new Item("name5", "desc5", 5);
         tracker.add(item2);
-        String[] three = {"3", "name4"};
-        Input input1 = new StubInput(three);
+        Input input1 = new StubInput(new String[]{"3", "name4"});
         new StartUI(input1, tracker).init();
         assertThat(tracker.getAll()[0].getName(), is("name5"));
 
@@ -61,11 +51,9 @@ public class StartUITest {
         Tracker tracker = new Tracker();
         Item item = new Item("name6", "desc6", 6);
         tracker.add(item);
-        String[] four = {"4", "name6", item.getId()};
-        Input input = new StubInput(four);
+        Input input = new StubInput(new String[]{"4", "name6", item.getId()});
         new StartUI(input, tracker).init();
         assertThat(tracker.getAll()[0].getName(), is("name6"));
-
     }
 
     @Test
@@ -73,35 +61,26 @@ public class StartUITest {
         Tracker tracker = new Tracker();
         Item item = new Item("name7", "desc7", 7);
         tracker.add(item);
-        String[] five = {"5", "name7"};
-        Input input = new StubInput(five);
+        Input input = new StubInput(new String[]{"5", "name7"});
         new StartUI(input, tracker).init();
         assertThat(tracker.getAll()[0].getName(), is("name7"));
-
     }
 
     @Test
     public void whenAddItemAndEditItem() {
-
         Item item = new Item("name2", "desc2", 2);
         Item item2 = new Item("name3", "desc3", 3);
         this.createAndAdd(item);
-        String[] two = {"2", "name2", "name3", "desc3"};
-        Input input = new StubInput(two);
+        Input input = new StubInput(new String[]{"2", "name2", "name3", "desc3"});
         new StartUI(input, this.createAndAdd(item)).init();
         assertThat(this.createAndAdd(item2).getAll()[0].getName(), is("name3"));
-
     }
 
     @Test
     public void whenAddItemAndShowItemByName() {
-
         Item item = new Item("name2", "desc2", 9);
-        String[] seven  = {"5", "name2"};
-        Input input = new StubInput(seven);
+        Input input = new StubInput(new String[]{"5", "name2"});
         new StartUI(input, this.createAndAdd(item)).init();
         assertThat(this.createAndAdd(item).getAll()[0].getName(), is("name2"));
-
     }
-
 }
