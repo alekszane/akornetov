38e87c8ff99ba1464329187f065316a3660943a7
Aleksey <all-1313@yandex.ru>
2018-04-28 01:30:00 +0300

    Рефакторинг кофе машины

--- chapter_002/src/main/java/ru/job4j/coffemachine/CoffeMachine.java
+++ chapter_002/src/main/java/ru/job4j/coffemachine/CoffeMachine.java
@@ -10,13 +10,8 @@ import java.util.Arrays;
 public class CoffeMachine {
 
 	private int value; //Купюра 50, 100 руб.
-
 	private int price; // Цена на кофе.
-
-	private int coin10, coin5, coin2, coin1; // Монеты.
-
-	//private int[] nominalCoin = {10, 5, 2, 1};
-
+	private int[] coins = {10, 5, 2, 1}; // Массив номиналов монет.
 
 	public CoffeMachine(int value, int price) {
 		this.value = value;
@@ -31,76 +26,21 @@ public class CoffeMachine {
 		return this.price;
 	}
 
-	public int[] changes(int value, int price) { //
-
-		int handing = value - price; //Колличество денег для выдачи
-
-		//Разбиваем сдачу на монеты.
-
-				if (handing >= 10) {
-					coin10 = handing / 10;
-					handing -= (coin10 * 10);
-				}
-				if (handing >= 5) {
-					coin5 = handing / 5;
-					handing -= (coin5 * 5);
-				}
-				if (handing >= 2) {
-					coin2 = handing / 2;
-					handing -= (coin2 * 2);
-				}
-				 if (handing == 1) {
-			coin1 = handing;
-				}
-
-		int countCoin = coin10 + coin5 + coin2 + coin1; // Считаем колличество монет.
-
-		int[] numCoin = new int[countCoin]; // Массив с монетами для выдачи.
-
-			// Заполняем ячейки массива
-		if (coin10 != 0) {
-				for (int i = 0; i < numCoin.length; i++) {
-						numCoin[i] = 10;
-						if (i == coin10 - 1) {
-						break;
-						}
-					}
-				}
-		if (coin5 != 0) {
-			for (int i = coin10; i < numCoin.length; i++) {
-				numCoin[i] = 5;
-				if (i == coin10 + coin5) {
-					break;
-				}
+	public int[] changes(int value, int price) {
+		int handing = value - price; //Колличество денег для выдачи.
+		int[] result = new int[5]; // Массив возвращаемых монет.
+		int j = 0;
+		for (int i : coins) {
+			while (handing - i >= 0) {
+				handing -= i;
+				result[j] = i;
+				j++;
 			}
 		}
-		if (coin2 != 0) {
-			for (int i = coin10 + coin5; i < numCoin.length; i++) {
-				numCoin[i] = 2;
-				if (i == coin10 + coin5 + coin2) {
-					break;
-				}
-			}
-		}
-		if (coin1 != 0) {
-			for (int i = coin10 + coin5 + coin2; i < numCoin.length; i++) {
-				numCoin[i] = 1;
-				if (i == coin10 + coin5 + coin2 + coin1) {
-					break;
-				}
-			}
-		}
-				Arrays.sort(numCoin); // Сортируем массив
-
-				for (int x = 0; x < numCoin.length / 2; x++) { //Выводим в обр. порядке
-					int tmp = numCoin[x];
-					numCoin[x] = numCoin[numCoin.length - 1 - x];
-					numCoin[numCoin.length - 1 - x] = tmp;
-				}
-
-			return 	numCoin;
+		return result;
 	}
 }
+
 
 
 
