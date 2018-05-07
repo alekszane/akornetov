ab01f973f2157a21097b9cf05a902dd07033fb68
Aleksey <all-1313@yandex.ru>
2018-05-08 01:38:33 +0300

    Добавил тест, отрефакторил методы

--- chapter_003/src/main/java/ru/job4j/collections/banktransfer/BankTransfer.java
+++ chapter_003/src/main/java/ru/job4j/collections/banktransfer/BankTransfer.java
@@ -12,28 +12,37 @@ public class BankTransfer {
 
 	private  Map<User, List<Account>> userBank = new HashMap<>(); //Хранилище банковских счетов
 
-
-	/*
-		*Метод добавляет пользователя в хранилище
-		*@param user
-		*/
+	/**
+	 *Метод добавляет пользователя в хранилище
+	 *@param user
+	 */
 	public void addUser(User user) {
 		userBank.putIfAbsent(user, user.getUserAccount());
 	}
-	/*
+
+	/**
 	*Метод удаляет пользователя из хранилища
-	*@param user
+	* @param user
 	*/
 	public void deleteUser(User user) {
 			userBank.remove(user);
 	}
 
-	public void deleteAccountFromUser(String passport, Account account) { //- удалить один счёт пользователя.
-
+	/**
+	*Метод удаляет пользователя из хранилища
+	* @param passport
+	* @param account
+	*/
+	public void deleteAccountFromUser(String passport, Account account) {
 			getUserAccounts(passport).remove(account);
 	}
 
-	public List<Account> getUserAccounts(String passport) { //- получить список счетов для пользователя.
+	/**
+	 * Метод позволяет получить список счетов для пользователя.
+	 * @param passport
+	 * @return Account
+	 */
+	public List<Account> getUserAccounts(String passport) {
 		List<Account> userAkk = new ArrayList<>();
 		for (User key : userBank.keySet()) {
 			if (key.getPassport().equals(passport)) {
@@ -43,42 +52,55 @@ public class BankTransfer {
 		}
 		return userAkk;
 	}
-	//- метод для перечисления денег с одного счёта на другой счёт:
-	//если счёт не найден или не хватает денег на счёте srcAccount (с которого переводят) должен вернуть false.
+	/* метод для  поиска аккаунта по реквизитам.
+	* @param requisite.
+	* @return Account.
+	*/
+	private Account searchAccountToRequisite(String requisite) {
+		Collection<List<Account>> value = userBank.values();
+		Account account = new Account(" ", 0);
+
+		for (List<Account> list : value) {
+			for (Account acc : list) {
+					if (acc.getRequisit().equals(requisite)) {
+						account = acc;
+					}
+				}
+			}
+			return account;
+	}
+	/** метод для перечисления денег с одного счёта на другой счёт:
+	* если счёт не найден или не хватает денег на счёте srcAccount (с которого переводят) должен вернуть false.
+	* @param srcPassport
+	* @param srcRequisite
+	* @param destPassport
+	* @param destRequisite
+	* @param amount
+	* @return boolean
+	*/
 	public boolean transferMoney(String srcPassport, String srcRequisite, String destPassport, String destRequisite, int amount) {
 		boolean result = false;
 		Set<User> keys = userBank.keySet();
-		Collection<List<Account>> value = userBank.values();
+		Account account;
 
 			for (User userSrc : keys) {
-				if (userBank.containsKey(userSrc) && userSrc.getPassport().equals(srcPassport)) {
-						for (List<Account> list : value) {
-							for (Account acc : list) {
-								if (acc.getRequisit().equals(srcRequisite) && acc.getValue() >= amount) {
-									int removeMoney = acc.getValue() - amount;
-									acc.setValue(removeMoney);
-									int numberAccount = list.indexOf(acc); //Номер аккаунта в листе.
-									list.set(numberAccount, acc);
-									userBank.putIfAbsent(userSrc, list);
-								}
-							}
-						}
-					}
-				if (userBank.containsKey(userSrc) && userSrc.getPassport().equals(destPassport)) {
-					for (List<Account> list : value) {
-						for (Account acc : list) {
-							if (acc.getRequisit().equals(destRequisite) && (acc.getValue() + amount) > 0) {
-								int removeMoney = acc.getValue() + amount;
-								acc.setValue(removeMoney);
-								int numberAccount = list.indexOf(acc); //Номер аккаунта в листе.
-								list.set(numberAccount, acc);
-								userBank.putIfAbsent(userSrc, list);
-								result = true;
-							}
-						}
+				if (userSrc.getPassport().equals(srcPassport)) {
+					account = searchAccountToRequisite(srcRequisite);
+					if ((account.getValue() - amount) > 0) {
+						int removeMoney = account.getValue() - amount;
+						account.setValue(removeMoney);
 					}
 				}
 
+				if (userSrc.getPassport().equals(destPassport)) {
+					account = searchAccountToRequisite(destRequisite);
+					if ((account.getValue() + amount) > 0) {
+						int addMoney = account.getValue() + amount;
+						account.setValue(addMoney);
+						result = true;
+						System.out.println(account.getValue());
+					}
+				}
 			}
 		return  result;
 	}
