dcceb2cb40dfe72666f52277c3715b8b338633a9
alekszane <all-1313@yandex.ru>
2018-05-22 03:18:41 +0300

    Add Convert Iterator<Iterator>

---
+++ junior_001/src/main/java/ru/job4j/iterator/Converter.java
@@ -0,0 +1,10 @@
+package ru.job4j.iterator;
+
+/**
+ * @author Aleksey Kornetov (all-1313@yandex.ru)
+ *         project junior
+ *         Created on 21.05.2018.
+ */
+public class Converter {
+
+}
---
+++ junior_001/src/test/java/ru/job4j/iterator/ConverterTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+
+/**
+ * @author Aleksey Kornetov (all-1313@yandex.ru)
+ *         project junior
+ *         Created on 21.05.2018.
+ */
+public class ConverterTest {
+
+}
