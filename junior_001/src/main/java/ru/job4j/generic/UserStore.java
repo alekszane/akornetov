214c8b22bbd911dabb48260dede07db2fb0654f9
alekszane <all-1313@yandex.ru>
2018-05-29 23:55:36 +0300

    realize Store<T extends Base>

---
+++ junior_001/src/main/java/ru/job4j/generic/UserStore.java
@@ -0,0 +1,10 @@
+package ru.job4j.generic;
+
+/**
+ * @author Aleksey Kornetov (all-1313@yandex.ru)
+ *         project junior
+ *         Created on 28.05.2018.
+ */
+public class UserStore {
+
+}
---
+++ junior_001/src/test/java/ru/job4j/generic/RoleStoreTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+
+/**
+ * @author Aleksey Kornetov (all-1313@yandex.ru)
+ *         project junior
+ *         Created on 29.05.2018.
+ */
+public class RoleStoreTest {
+
+}
